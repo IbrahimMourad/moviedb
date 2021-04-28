e13be99e7d3acee518c1f427f50077d9ec70e3d2
Ibrahim Mohamed <ibrahimm.mourad97@gmail.com>
2021-04-28 22:30:27 +0200

    fix bug

--- src/components/AddMovie.js
+++ src/components/AddMovie.js
@@ -2,16 +2,12 @@ import React, { Component } from "react";
 import { Form, Button } from "react-bootstrap";
 import "./AddMovie.css";
 class AddMovie extends Component {
-  constructor(props) {
-    super(props);
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
   state = {
-    movieName: "Enter Name",
-    title: "Enter Title",
-    posterURL: "Enter URL",
+    movieName: "",
+    title: "",
+    posterURL: "",
   };
-  handleInputChange(event) {
+  handleInputChange = (event) => {
     const target = event.target;
     const value = target.value;
     const name = target.name;
@@ -19,7 +15,7 @@ class AddMovie extends Component {
     this.setState({
       [name]: value,
     });
-  }
+  };
 
   render() {
     return (
